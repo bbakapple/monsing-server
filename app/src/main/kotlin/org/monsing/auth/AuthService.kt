76a9fe9ffc7a37aa1fecc931fb44a7f3b1689299
kevstevie <jianna442@gmail.com>
2024-11-26 17:33:11 +0900

    feat: 가입처리 1차구현

---
+++ app/src/main/kotlin/org/monsing/auth/AuthService.kt
@@ -0,0 +1,42 @@
+package org.monsing.auth
+
+import org.monsing.jwt.TokenManager
+import org.monsing.jwt.TokenPayload
+import org.monsing.member.GenderType
+import org.monsing.member.Member
+import org.monsing.member.MemberRepository
+import org.monsing.member.MemberType
+import org.monsing.member.Nickname
+import org.monsing.member.OauthProviderType
+import org.springframework.stereotype.Service
+import java.net.URL
+
+@Service
+class AuthService(
+    private val memberRepository: MemberRepository,
+    private val oauthAdaptor: OauthAdaptor,
+    private val tokenManager: TokenManager
+) {
+
+    fun login(oauthProviderType: OauthProviderType, oauthToken: String): TokenResponse {
+        val oauthInfo = oauthAdaptor.handle(oauthProviderType, oauthToken)
+        val member = memberRepository.findByIdentifierAndOauthProviderType(oauthInfo.id, oauthProviderType)
+            ?: memberRepository.save(
+                Member(
+                    identifier = oauthInfo.id,
+                    oauthProviderType = oauthProviderType,
+                    memberType = MemberType.STUDENT,
+                    nickname = Nickname("dd"),
+                    genderType = GenderType.OTHER,
+                    profileImage = URL("asd")
+                )
+            )
+
+        val id = member.id ?: throw IllegalStateException("Member id must not be null")
+
+        return TokenResponse(
+            accessToken = tokenManager.createAccessToken(TokenPayload(id)),
+            refreshToken = tokenManager.createRefreshToken(id)
+        )
+    }
+}
