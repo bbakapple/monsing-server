ebbd54799a9c3f4a197fce498f86a2860fe94d8a
kevstevie <jianna442@gmail.com>
2024-11-26 16:14:24 +0900

    feat: token encode decode

--- app/src/main/kotlin/org/monsing/jwt/TokenManager.kt
+++ app/src/main/kotlin/org/monsing/jwt/TokenManager.kt
@@ -1,30 +1,48 @@
 package org.monsing.jwt
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import java.util.*
 import javax.crypto.SecretKey
 
 @Component
 class TokenManager(
-    @Value("\${jwt.access-key}") private val key: SecretKey
+    @Value("\${jwt.access-key}") accessKey: String,
+    @Value("\${jwt.expire-second}") private val expireSecond: Long,
+    private val objectMapper: ObjectMapper
 ) {
 
-    private val parser = Jwts.parser()
-        .verifyWith(key)
-        .build()
+    private val key: SecretKey = Keys.hmacShaKeyFor(accessKey.toByteArray())
 
     fun getPayLoad(token: String): TokenPayload {
+        val parser = Jwts.parser()
+            .verifyWith(key)
+            .build()
         try {
             return parser.parseSignedClaims(token)
                 .payload
-                .let { TokenPayload(it["id"] as Long) }
+                .subject
+                .let { objectMapper.readValue(it, TokenPayload::class.java) }
         } catch (e: ExpiredJwtException) {
             throw ExpiredJwtException(e.header, e.claims, e.message)
         } catch (e: Exception) {
             throw IllegalArgumentException("Invalid token")
         }
+    }
+
+    fun createToken(payload: TokenPayload): String {
+        val issuedAt = Date()
+        val expiration = issuedAt.toInstant().plusSeconds(expireSecond).let { Date.from(it) }
+        return Jwts.builder()
+            .subject(payload.id.toString())
+            .issuedAt(issuedAt)
+            .expiration(expiration)
+            .signWith(key)
+            .compact()
     }
 }
 
---
+++ app/src/test/kotlin/org/monsing/jwt/TokenManagerTest.kt
@@ -0,0 +1,19 @@
+package org.monsing.jwt
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class TokenManagerTest : StringSpec({
+
+    "토큰 페이로드 검증" {
+        val key = "sfamamowevamov2mo2o42ov4momoomvm4osdmfkadll"
+        val tokenManager = TokenManager(key, 3600L, ObjectMapper())
+
+        val token = tokenManager.createToken(TokenPayload(1L))
+
+        val payload = tokenManager.getPayLoad(token)
+
+        payload.id shouldBe 1L
+    }
+})
