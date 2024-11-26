0010a867f8e80c250ef6e9188fb76724decfccb1
kevstevie <jianna442@gmail.com>
2024-11-26 12:53:24 +0900

    refactor: 개행

--- api/src/main/kotlin/org/monsing/auth/AuthConfig.kt
+++ api/src/main/kotlin/org/monsing/auth/AuthConfig.kt
@@ -5,7 +5,9 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class AuthConfig(private val authInterceptor: AuthInterceptor) : WebMvcConfigurer {
+class AuthConfig(
+    private val authInterceptor: AuthInterceptor
+) : WebMvcConfigurer {
 
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(authInterceptor)
