c371c083afbb0265315ad8619b74457306739c9f
kevstevie <jianna442@gmail.com>
2024-11-27 20:54:18 +0900

    refactor: 주입 방식 수정

--- app/src/main/kotlin/org/monsing/auth/oauth/OauthAdaptor.kt
+++ app/src/main/kotlin/org/monsing/auth/oauth/OauthAdaptor.kt
@@ -1,26 +1,17 @@
 package org.monsing.auth.oauth
 
-import jakarta.annotation.PostConstruct
 import org.monsing.auth.oauthhandler.OauthHandler
 import org.monsing.auth.oauthhandler.OauthIdentifier
 import org.monsing.member.OauthProviderType
-import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.stereotype.Component
 
 @Component
-class OauthAdaptor {
-
-    private val handlers = mutableListOf<OauthHandler>()
+class OauthAdaptor(
+    private val handlers: List<OauthHandler>
+) {
 
     fun handle(oauthProviderType: OauthProviderType, oauthToken: String): OauthIdentifier {
         return handlers.firstOrNull { it.canHandle(oauthProviderType) }?.handle(oauthToken)
             ?: throw IllegalArgumentException("Unsupported oauth provider type: $oauthProviderType")
-    }
-
-    @PostConstruct
-    fun init() {
-        AnnotationConfigApplicationContext()
-            .getBeansOfType(OauthHandler::class.java)
-            .values.forEach { handlers.add(it) }
     }
 }
