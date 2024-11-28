59a27e04620c71f459829c754f05615972aca4c1
kevstevie <jianna442@gmail.com>
2024-11-28 23:51:19 +0900

    refactor: token vo 작성 및 response 모듈 수정

--- app/src/main/kotlin/org/monsing/auth/AuthService.kt
+++ app/src/main/kotlin/org/monsing/auth/AuthService.kt
@@ -9,6 +9,7 @@ import org.monsing.member.MemberRepository
 import org.monsing.member.MemberType
 import org.monsing.member.Nickname
 import org.monsing.member.OauthProviderType
+import org.monsing.token.Token
 import org.springframework.stereotype.Service
 import java.net.URL
 
@@ -19,7 +20,7 @@ class AuthService(
     private val tokenManager: TokenManager
 ) {
 
-    fun login(oauthProviderType: OauthProviderType, oauthToken: String): TokenResponse {
+    fun login(oauthProviderType: OauthProviderType, oauthToken: String): Token {
         val oauthIdentifier = oauthAdaptor.handle(oauthProviderType, oauthToken)
         val member = memberRepository.findByIdentifierAndOauthProviderType(oauthIdentifier.id, oauthProviderType)
             ?: throw IllegalArgumentException("Member not found")
@@ -28,13 +29,13 @@ class AuthService(
             "Member id must not be null"
         }
 
-        return TokenResponse(
+        return Token(
             accessToken = tokenManager.createAccessToken(TokenPayload(id)),
             refreshToken = tokenManager.createRefreshToken(id)
         )
     }
 
-    fun register(registerRequest: RegisterRequest): TokenResponse {
+    fun register(registerRequest: RegisterRequest): Token {
         val oauthIdentifier = oauthAdaptor.handle(
             OauthProviderType.valueOf(registerRequest.oauthProviderType),
             registerRequest.oauthToken
@@ -55,16 +56,16 @@ class AuthService(
             "Member id must not be null"
         }
 
-        return TokenResponse(
+        return Token(
             accessToken = tokenManager.createAccessToken(TokenPayload(id)),
             refreshToken = tokenManager.createRefreshToken(id)
         )
     }
 
-    fun refresh(refreshToken: String): TokenResponse {
+    fun refresh(refreshToken: String): Token {
         val payload = tokenManager.getRefreshPayload(refreshToken)
 
-        return TokenResponse(
+        return Token(
             accessToken = tokenManager.createAccessToken(TokenPayload(payload)),
             refreshToken = refreshToken
         )
---
+++ domain/src/main/kotlin/org/monsing/token/Token.kt
@@ -0,0 +1,6 @@
+package org.monsing.token
+
+data class Token(
+    val accessToken: String,
+    val refreshToken: String
+)
