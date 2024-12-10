ae66b7aa011992b9ab2fbfaeb97a618168b31206
kevstevie <jianna442@gmail.com>
2024-12-10 14:54:06 +0900

    feat: run application

---
+++ chat/src/main/kotlin/org/monsing/MonsingChatApplication.kt
@@ -0,0 +1,11 @@
+package org.monsing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MonsingChatApplication
+
+fun main() {
+    runApplication<MonsingChatApplication>()
+}
