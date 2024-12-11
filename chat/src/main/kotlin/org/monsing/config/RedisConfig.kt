6cbc62fa31020d43fc2b231d7aa8174811e0138c
kevstevie <jianna442@gmail.com>
2024-12-11 12:20:42 +0900

    refactor: bean 등록

--- chat/src/main/kotlin/org/monsing/config/RedisConfig.kt
+++ chat/src/main/kotlin/org/monsing/config/RedisConfig.kt
@@ -1,20 +1,24 @@
 package org.monsing.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.serializer.GenericToStringSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
 class RedisConfig {
 
+    @Bean
     fun connectionFactory() = LettuceConnectionFactory().apply {
         //TODO: 레디스 설정
     }
 
-    fun redisTemplate() = RedisTemplate<String, String>().apply {
+    @Bean
+    fun redisTemplate() = RedisTemplate<Long, String>().apply {
         connectionFactory = connectionFactory()
-        keySerializer = StringRedisSerializer()
+        keySerializer = GenericToStringSerializer(Long::class.java)
         valueSerializer = StringRedisSerializer()
     }
 }
