c493b6a1368686c4cf18bd5fa809277309f6a6dd
kevstevie <jianna442@gmail.com>
2024-12-14 07:38:17 +0900

    refactor: 의존관계 수정

--- chat/src/main/kotlin/org/monsing/application/ChatService.kt
+++ chat/src/main/kotlin/org/monsing/application/ChatService.kt
@@ -1,14 +1,113 @@
 package org.monsing.application
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import org.monsing.domain.Chat
+import org.monsing.domain.MemberChat
+import org.monsing.domain.MemberChatRepository
 import org.monsing.domain.Message
+import org.monsing.domain.MessageRepository
+import org.monsing.domain.session.GlobalServerIdStorage
+import org.monsing.domain.session.LocalSessionStorage
 import org.springframework.stereotype.Service
+import org.springframework.web.socket.TextMessage
+import org.springframework.web.socket.WebSocketMessage
+import org.springframework.web.socket.WebSocketSession
 
 @Service
 class ChatService(
-    private val webSocketHandler: WebSocketHandler
+    private val localSessionStorage: LocalSessionStorage,
+    private val globalServerIdStorage: GlobalServerIdStorage,
+    private val memberChatRepository: MemberChatRepository,
+    private val messageRepository: MessageRepository,
+    private val objectMapper: ObjectMapper
 ) {
 
     fun relayMessage(receiverId: Long, message: Message) {
-        webSocketHandler.relayMessage(receiverId, message)
+        localSessionStorage.getSession(receiverId)?.let {
+            it.forEach {
+                it.sendMessage(message.toPayload())
+            }
+        } ?: publishMessageSentEvent()
     }
+
+    private fun publishMessageSentEvent() {
+        TODO("Not yet implemented")
+    }
+
+    fun createChat(memberIds: List<Long>): String {
+        val chat = memberChatRepository.saveChat(Chat())
+        val chatId = chat.id
+
+        memberIds.forEach { memberId ->
+            joinChat(chatId, memberId)
+        }
+        return chatId
+    }
+
+    fun joinChat(chatId: String, memberId: Long) {
+        memberChatRepository.save(MemberChat(chatId = chatId, memberId = memberId))
+    }
+
+    fun leaveChat(chatId: String, memberId: Long) {
+        memberChatRepository.deleteByChatIdAndMemberId(chatId, memberId)
+    }
+
+
+    fun saveSession(session: WebSocketSession) {
+        session.attributes["memberId"]?.let {
+            localSessionStorage.saveSession(it as Long, session)
+            globalServerIdStorage.saveServerId(it, session.localAddress.toString())
+        }
+    }
+
+    fun removeSession(session: WebSocketSession) {
+        session.attributes["memberId"]?.let {
+            localSessionStorage.removeSession(it as Long, session)
+            globalServerIdStorage.removeServerId(it, session.localAddress.toString())
+        }
+    }
+
+    fun handleMessage(senderId: Long, message: WebSocketMessage<*>) {
+        //TODO: message DTO로 수정해야함
+        val msg = objectMapper.readValue(message.payload as String, Message::class.java)
+
+        messageRepository.save(msg)
+
+        sendMessage(msg)
+    }
+
+
+    private fun sendMessage(message: Message) {
+        val receivers = memberChatRepository.findReceiverIdByChatId(message.chatId, message.senderId)
+
+        for (receiver in receivers) {
+            localSessionStorage.getSession(receiver)?.let {
+                it.forEach {
+                    it.sendMessage(message.toPayload())
+                }
+            } ?: globalServerIdStorage.getServerId(receiver)?.let {
+                it.forEach {
+                    sendOtherServer(it, receiver, message)
+                }
+            } ?: publishMessageSentEvent()
+        }
+    }
+
+    private fun sendOtherServer(receiverServerId: String, receiverId: Long, message: Message) {
+        val client = HttpClient.newHttpClient()
+        client.sendAsync(
+            HttpRequest.newBuilder()
+                .uri(URI.create("http:/$receiverServerId/relay?receiverId=$receiverId"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(message)))
+                .build(),
+            HttpResponse.BodyHandlers.ofString()
+        )
+    }
+
+    private fun Message.toPayload() = TextMessage(objectMapper.writeValueAsString(this))
 }
--- chat/src/main/kotlin/org/monsing/application/WebSocketHandler.kt
+++ chat/src/main/kotlin/org/monsing/application/WebSocketHandler.kt
@@ -1,91 +1,29 @@
 package org.monsing.application
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import org.monsing.domain.Message
-import org.monsing.domain.MessageRepository
-import org.monsing.domain.session.GlobalServerIdStorage
-import org.monsing.domain.session.LocalSessionStorage
 import org.springframework.stereotype.Component
 import org.springframework.web.socket.CloseStatus
-import org.springframework.web.socket.TextMessage
 import org.springframework.web.socket.WebSocketMessage
 import org.springframework.web.socket.WebSocketSession
 import org.springframework.web.socket.handler.TextWebSocketHandler
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
 
 @Component
 class WebSocketHandler(
-    private val globalServerIdStorage: GlobalServerIdStorage,
-    private val localSessionStorage: LocalSessionStorage,
-    private val objectMapper: ObjectMapper,
-    private val messageRepository: MessageRepository
+    private val chatService: ChatService
 ) : TextWebSocketHandler() {
 
     override fun afterConnectionEstablished(session: WebSocketSession) {
-        session.attributes["memberId"]?.let {
-            localSessionStorage.saveSession(it as Long, session)
-            globalServerIdStorage.saveServerId(it, session.localAddress.toString())
-        }
+        chatService.saveSession(session)
     }
 
     override fun handleMessage(session: WebSocketSession, message: WebSocketMessage<*>) {
-        val msg = objectMapper.readValue(message.payload as String, Message::class.java)
+        val senderId = requireNotNull(session.attributes["memberId"] as Long)
 
-        messageRepository.save(msg)
-
-        sendMessage(msg)
+        chatService.handleMessage(senderId, message)
     }
 
     override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
-        session.attributes["memberId"]?.let {
-            localSessionStorage.removeSession(it as Long, session)
-            globalServerIdStorage.removeServerId(it, session.localAddress.toString())
-        }
+        chatService.removeSession(session)
     }
-
-    private fun sendMessage(message: Message) {
-        val receivers = messageRepository.findReceiverIdByChatId(message.chatId, message.senderId)
-
-        for (receiver in receivers) {
-            localSessionStorage.getSession(receiver)?.let {
-                it.forEach {
-                    it.sendMessage(message.toPayload())
-                }
-            } ?: globalServerIdStorage.getServerId(receiver)?.let {
-                it.forEach {
-                    sendOtherServer(it, receiver, message)
-                }
-            } ?: alert(receiver)
-        }
-    }
-
-    fun relayMessage(receiverId: Long, message: Message) {
-        val sessions = localSessionStorage.getSession(receiverId)
-
-        sessions?.forEach {
-            it.sendMessage(message.toPayload())
-        }
-    }
-
-    private fun sendOtherServer(receiverServerId: String, receiverId: Long, message: Message) {
-        val client = HttpClient.newHttpClient()
-        client.sendAsync(
-            HttpRequest.newBuilder()
-                .uri(URI.create("http:/$receiverServerId/relay?receiverId=$receiverId"))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(objectMapper.writeValueAsString(message)))
-                .build(),
-            HttpResponse.BodyHandlers.ofString()
-        )
-    }
-
-    fun alert(receiverId: Long) {
-        //TODO:알림 전송
-    }
-
-    private fun Message.toPayload() = TextMessage(objectMapper.writeValueAsString(this))
 }
+
 
