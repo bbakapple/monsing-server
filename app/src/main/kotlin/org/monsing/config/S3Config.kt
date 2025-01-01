f333d6e94a199bcef6cfce0de984a38e2a650d4c
kevstevie <jianna442@gmail.com>
2025-01-01 22:13:30 +0900

    feat: s3 client 설정

---
+++ app/src/main/kotlin/org/monsing/config/S3Config.kt
@@ -0,0 +1,17 @@
+package org.monsing.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import software.amazon.awssdk.auth.credentials.InstanceProfileCredentialsProvider
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.s3.S3Client
+
+@Configuration
+class S3Config {
+
+    @Bean
+    fun s3Client(): S3Client = S3Client.builder()
+        .region(Region.AP_NORTHEAST_2)
+        .credentialsProvider(InstanceProfileCredentialsProvider.create())
+        .build()
+}
