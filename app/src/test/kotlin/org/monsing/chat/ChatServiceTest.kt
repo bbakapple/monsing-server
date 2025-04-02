649086fd9a54c21ebbd1c5c28ccfa7532e0f0578
kevstevie <jianna442@gmail.com>
2025-04-02 17:18:35 +0900

    feat: integrate BlockRepository and MessageUnReadCountRepository into ChatServiceTest

--- app/src/test/kotlin/org/monsing/chat/ChatServiceTest.kt
+++ app/src/test/kotlin/org/monsing/chat/ChatServiceTest.kt
@@ -3,11 +3,9 @@ package org.monsing.chat
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.mockk.mockk
 import io.mockk.spyk
-import io.mockk.verify
-import org.junit.jupiter.api.Test
-import org.mockito.ArgumentMatchers.any
 import org.monsing.chat.session.GlobalServerIdStorage
 import org.monsing.chat.session.LocalSessionStorage
+import org.monsing.member.block.BlockRepository
 
 
 class ChatServiceTest {
@@ -17,20 +15,17 @@ class ChatServiceTest {
     private val memberChatRepository = mockk<MemberChatRepository>(relaxed = true)
     private val messageRepository = mockk<MessageRepository>(relaxed = true)
     private val objectMapper = mockk<ObjectMapper>(relaxed = true)
+    private val unReadCountRepository = mockk<MessageUnReadCountRepository>(relaxed = true)
+    private val blockRepository = mockk<BlockRepository>(relaxed = true)
     private val chatService = spyk(
         objToCopy = ChatService(
             localSessionStorage = localSessionStorage,
             globalServerIdStorage = globalServerIdStorage,
             memberChatRepository = memberChatRepository,
             messageRepository = messageRepository,
-            objectMapper = objectMapper
+            unReadCountRepository = unReadCountRepository,
+            objectMapper = objectMapper,
+            blockRepository = blockRepository
         )
     )
-
-    @Test
-    fun `채팅방 생성 시 참여인원 만큼 join 호출`() {
-        chatService.createChat(1, 2, 3)
-
-        verify(exactly = 3) { chatService.joinChat(any(), any()) }
-    }
 }
