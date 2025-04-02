de50940a0fc13fee2f07740f3b260323e9c8f605
kevstevie <jianna442@gmail.com>
2025-04-02 17:56:23 +0900

    feat: refine AuthPayload annotation and update detekt configuration for parameter naming and exclusions

--- domain/src/main/kotlin/org/monsing/member/teacher/TeacherRepository.kt
+++
@@ -1,8 +0,0 @@
-package org.monsing.member.teacher
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface TeacherRepository : JpaRepository<Teacher, Long>, TeacherCustomRepository {
-}
